dfec0bbd201db985c3aa225b064750df4aa57621
amlo026 <xiao.la0820@gmail.com>
2025-06-13 12:12:59 -0400

    Update dream_visualizer.py

--- dream_visualizer.py
+++ dream_visualizer.py
@@ -17,6 +17,9 @@ Original file is located at
 #making it easier to remember, understand, and express those experiences."
 
 #Import statements
+!pip install nest_asyncio -q
+!pip install google-generativeai -q
+!pip install Pillow -q
 from google import genai
 from google.genai import types
 from PIL import Image
